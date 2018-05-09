29186a241870686c704ff5967e32f9223ac2ef22
ghnjk <ghnjk@github.com>
2018-05-10 00:08:57 +0800

    增加流解析

--- simple_flow/flow.py
+++ simple_flow/flow.py
@@ -57,7 +57,7 @@ def add_flow(src, op, name):
     在src节点后增加op形成新的神经元dst
     :param src: 源节点
     :param op: 操作
-    ;param name: 新节点名字
+    :param name: 新节点名字
     :return: dst
     """
     dst = Neurons(name=name)
@@ -65,3 +65,58 @@ def add_flow(src, op, name):
     dst.pre_list.append(e)
     src.next_list.append(e)
     return dst
+
+
+class NetWork(object):
+
+    def __init__(self):
+        self.in_nodes = []
+        self.out_node = None
+        self.flow_nodes = []
+        self.node_id_sets = set()
+        self.mpNode = {}
+
+    def parse(self, loss_node):
+        self.in_nodes = []
+        self.flow_nodes = []
+        self.mpNode = {}
+        self.node_id_sets = set()
+        self.out_node = loss_node
+        self._dfs_search_in_nodes(self.out_node)
+        self._bfs_search_flows()
+
+    def _dfs_search_in_nodes(self, node):
+        node_id = id(node)
+        if node_id in self.mpNode.keys():
+            return
+        self.mpNode[node_id] = {
+            "node": node,
+            "in_d": len(node.pre_list)
+        }
+        self.node_id_sets.add(node_id)
+        if self.mpNode[node_id]["in_d"] == 0:
+            self.in_nodes.append(node)
+        for p in node.pre_list:
+            self._dfs_search_in_nodes(p)
+
+    def _bfs_search_flows(self):
+        q = []
+        for node in self.in_nodes:
+            node_id = id(node)
+            q.append(node)
+            self.flow_nodes.append(node)
+            del self.mpNode[node_id]
+        while len(q) > 0:
+            node = q.pop(0)
+            for e in node.next_list:
+                c = e.dst_node
+                c_id = id(c)
+                if c_id not in self.node_id_sets:
+                    continue
+                self.mpNode[c_id]["in_d"] -= 1
+                if self.mpNode[c_id]["in_d"] == 0:
+                    q.append(c)
+                    self.flow_nodes.append(c)
+                    del self.mpNode[c_id]
+        if len(self.mpNode.keys()) != 0:
+            raise Exception("invalid graph.may circle nodes in the network.")
---
+++ tests/flow_test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import numpy as np
+
+
+def test_network_parse()
+    pass
